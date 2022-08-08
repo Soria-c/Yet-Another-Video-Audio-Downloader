575712f185618f102e427bb7fa2db12fe2daf8d4
Carlos Soria Otárola <98144875+Soria-c@users.noreply.github.com>
2022-08-08 00:00:38 -0500

    Update install.bat

--- install.bat
+++ install.bat
@@ -2,4 +2,4 @@
 curl --ssl-no-revoke -SL https://www.7-zip.org/a/7zr.exe -O
 curl --ssl-no-revoke -SL https://www.gyan.dev/ffmpeg/builds/ffmpeg-git-full.7z -O
 7zr.exe x ffmpeg-git-full.7z
-setx path "%PATH%;C:\Users\User\Desktop\New folder\ffmpeg-2022-08-03-git-d3f48e68b3-full_build\bin"
+setx path "%PATH%;%CD%\ffmpeg-2022-08-03-git-d3f48e68b3-full_build\bin"
