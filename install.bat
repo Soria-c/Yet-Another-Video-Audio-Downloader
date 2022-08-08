4daeaaa35320ecf4be4a5ff28aa2cb14718b303b
Carlos Soria <soriac367@gmail.com>
2022-08-07 23:56:45 -0500

    Added install script for windows

---
+++ install.bat
@@ -0,0 +1,5 @@
+@echo off
+curl --ssl-no-revoke -SL https://www.7-zip.org/a/7zr.exe -O
+curl --ssl-no-revoke -SL https://www.gyan.dev/ffmpeg/builds/ffmpeg-git-full.7z -O
+7zr.exe x ffmpeg-git-full.7z
+setx path "%PATH%;C:\Users\User\Desktop\New folder\ffmpeg-2022-08-03-git-d3f48e68b3-full_build\bin"
