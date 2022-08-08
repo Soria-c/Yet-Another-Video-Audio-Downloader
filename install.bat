7fa1e2271f4fbf81a0989d627199cde8a23fdfe6
Carlos Soria Otárola <98144875+Soria-c@users.noreply.github.com>
2022-08-08 00:33:05 -0500

    Update install.bat

--- install.bat
+++ install.bat
@@ -2,4 +2,7 @@
 curl --ssl-no-revoke -SL https://www.7-zip.org/a/7zr.exe -O
 curl --ssl-no-revoke -SL https://www.gyan.dev/ffmpeg/builds/ffmpeg-git-full.7z -O
 7zr.exe x ffmpeg-git-full.7z
-setx path "%PATH%;%CD%\ffmpeg-2022-08-03-git-d3f48e68b3-full_build\bin"
+setx path "%PATH%;%CD%"
+pip install -r requirements.txt
+playwright install chrome
+echo python main.py > pydl.bat
